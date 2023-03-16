3e37e794ab64be6160b6129d1e6e584bf1bd45e2
Drew Edwards <github@drew.contact>
2023-03-16 18:19:16 +0000

    feat: `pronouns` in `User` object

--- src/types/User.ts
+++ src/types/User.ts
@@ -6,13 +6,23 @@ export interface User {
      * the server may send anything. */
     group: "default" | "moderator" | "admin" | string;
 
+    /**
+     * The [pronouns](https://docs.sc3.io/faq/pronouns.html) set by the user by running `/pronouns`. This may be `null`
+     * if the player has not set any preferred pronouns. Where reasonably possible, you should attempt to use the user's
+     * preferred pronouns, or avoid using pronouns entirely. If you are unable to do this, you should use the player's
+     * name instead.
+     *
+     * @example "she/her"
+     */
+    pronouns: string | null;
+
     /** 
      * The world the player is in, or `null` if this information is not 
      * available. It will be a Minecraft namespaced registry key, for example:
      * 
      * - `minecraft:overworld` - The overworld
-     * - `minecraft:the_nether` - The nether
-     * - `minecraft:the_end` - The end
+     * - `minecraft:the_nether` - The Nether
+     * - `minecraft:the_end` - The End
      */
     world: string | null;
 
